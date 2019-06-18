7dc49527b91f56eb2fcd76b2c6c3674e329369e3
Matt Wilkinson <mytematt@gmail.com>
2019-06-18 11:02:52 -0600

    about me page work

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,7 +3,7 @@ import { AboutComponent } from './about/about.component';
 import { MyAppComponent } from './my-app.component';
 import { HomeComponent } from './home.component'
 
-export const appRoutes: Routes = [
+export const appRoutes:Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: '', pathMatch:'full', redirectTo: "/home"}]
